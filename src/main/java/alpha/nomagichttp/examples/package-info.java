ac8e247fcd04b9aabb03ef7a133b81ee1e4bde6a
Martin Andersson <webmaster@martinandersson.com>
2020-07-26 19:21:00 +0200

    Improve doc

--- src/main/java/alpha/nomagichttp/examples/package-info.java
+++ src/main/java/alpha/nomagichttp/examples/package-info.java
@@ -1,4 +1,6 @@
 /**
- * Runnable Java files that demonstrate the NoMagicHTTP library.
+ * Runnable Java files that demonstrate the NoMagicHTTP library.<p>
+ * 
+ * See the project's README.md file for console commands and output.
  */
 package alpha.nomagichttp.examples;
