210d729e7fc6ef84405ab3e0b85bc59fbe7b7314
Martin Andersson <webmaster@martinandersson.com>
2020-08-01 12:58:10 +0200

    Add large test

---
+++ src/test/large/java/alpha/nomagichttp/largetest/HelloLargeTest.java
@@ -0,0 +1,14 @@
+package alpha.nomagichttp.largetest;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Hello World large test.
+ */
+class HelloLargeTest
+{
+    @Test
+    void test() {
+        System.out.println("Hello");
+    }
+}
