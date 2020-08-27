8cdf18436f7250491d279e5b864848bb6d13aeac
Martin Andersson <webmaster@martinandersson.com>
2020-08-27 12:32:08 +0200

    Add some utility methods

--- src/main/java/alpha/nomagichttp/message/Responses.java
+++ src/main/java/alpha/nomagichttp/message/Responses.java
@@ -44,6 +44,51 @@ public final class Responses
         return ACCEPTED;
     }
     
+    public static Response badRequest() {
+        return new ResponseBuilder()
+                .httpVersion("HTTP/1.1")
+                .statusCode(400)
+                .reasonPhrase("Bad Request")
+                .mustCloseAfterWrite()
+                .noBody();
+    }
+    
+    public static Response notFound() {
+        return new ResponseBuilder()
+                .httpVersion("HTTP/1.1")
+                .statusCode(404)
+                .reasonPhrase("Not Found")
+                .mustCloseAfterWrite()
+                .noBody();
+    }
+    
+    public static Response entityTooLarge() {
+        return new ResponseBuilder()
+                .httpVersion("HTTP/1.1")
+                .statusCode(413)
+                .reasonPhrase("Entity Too Large")
+                .mustCloseAfterWrite()
+                .noBody();
+    }
+    
+    public static Response internalServerError() {
+        return new ResponseBuilder()
+                .httpVersion("HTTP/1.1")
+                .statusCode(500)
+                .reasonPhrase("Internal Server Error")
+                .mustCloseAfterWrite()
+                .noBody();
+    }
+    
+    public static Response notImplemented() {
+        return new ResponseBuilder()
+                .httpVersion("HTTP/1.1")
+                .statusCode(501)
+                .reasonPhrase("Not Implemented")
+                .mustCloseAfterWrite()
+                .noBody();
+    }
+    
     private static final Response OK = ResponseBuilder.ok().noBody();
     private static final Response ACCEPTED = ResponseBuilder.accepted().noBody();
 }
