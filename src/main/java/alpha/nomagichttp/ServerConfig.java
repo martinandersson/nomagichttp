a19f39ef47f3e025b853a93374651d07aefb6f3e
Martin Andersson <webmaster@martinandersson.com>
2020-08-27 12:16:38 +0200

    Add ServerConfig.maxErrorRecoveryAttempts

--- src/main/java/alpha/nomagichttp/ServerConfig.java
+++ src/main/java/alpha/nomagichttp/ServerConfig.java
@@ -39,4 +39,29 @@ public interface ServerConfig {
     default int maxRequestHeadSize() {
         return 8_000;
     }
+    
+    /**
+     * Returns the max number of attempts at recovering a failed request.<p>
+     * 
+     * This configuration has an effect only if the application has provided one
+     * or more exception handlers to the server and is then the max number of
+     * times the server is willing to attempt request recovery before giving
+     * up.<p>
+     * 
+     * When all tries have been exhausted, the {@link ExceptionHandler#DEFAULT
+     * default exception handler} will be called with the original exception.<p>
+     * 
+     * Successfully invoking an exception handler (handler returns a response or
+     * throws a <i>different</i> exception instance) counts as one attempt.<p>
+     * 
+     * @implSpec
+     * The default implementation returns {@code 5}.
+     * 
+     * @return max number of attempts
+     * 
+     * @see ExceptionHandler
+     */
+    default int maxErrorRecoveryAttempts() {
+        return 5;
+    }
 }
