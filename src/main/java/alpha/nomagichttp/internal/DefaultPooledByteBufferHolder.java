f1156980c2a14ce733158ab695e3bc765487937c
Martin Andersson <webmaster@martinandersson.com>
2020-10-29 16:32:11 +0100

    Constructor accepts afterRelease not onRelease

--- src/main/java/alpha/nomagichttp/internal/DefaultPooledByteBufferHolder.java
+++ src/main/java/alpha/nomagichttp/internal/DefaultPooledByteBufferHolder.java
@@ -5,6 +5,7 @@ import alpha.nomagichttp.message.PooledByteBufferHolder;
 import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.IntConsumer;
+import java.util.function.UnaryOperator;
 
 import static java.util.Objects.requireNonNull;
 
@@ -18,15 +19,39 @@ import static java.util.Objects.requireNonNull;
  */
 final class DefaultPooledByteBufferHolder implements PooledByteBufferHolder
 {
+    private static final IntConsumer NOOP = x -> {},
+                                     RELEASED = noopNew();
+    
+    private static IntConsumer noopNew() {
+        return new IntConsumer() {
+            @Override public void accept(int value) {
+                // Empty
+            }
+        };
+    }
+    
     private ByteBuffer buf;
     private volatile ByteBuffer view;
     private final int thenRemaining;
     private final AtomicReference<IntConsumer> onRelease;
+    private final IntConsumer afterRelease;
     
-    DefaultPooledByteBufferHolder(ByteBuffer buf, IntConsumer onRelease) {
+    /**
+     * Constructs a {@code DefaultPooledByteBufferHolder}.<p>
+     * 
+     * The {@code afterRelease} callback is executed after other callbacks
+     * possibly registered using the method {@link #onRelease(IntConsumer)}.
+     * {@code afterRelease} is guaranteed to be called even if an on-release
+     * callback throws an exception.
+     * 
+     * @param buf bytebuffer source
+     * @param afterRelease a sort of try-release-finally callback (may be {@code null})
+     */
+    DefaultPooledByteBufferHolder(ByteBuffer buf, IntConsumer afterRelease) {
         this.buf = view = buf;
         this.thenRemaining = buf.remaining();
-        this.onRelease = new AtomicReference<>(onRelease);
+        this.onRelease = new AtomicReference<>(NOOP);
+        this.afterRelease = afterRelease != null ? afterRelease : NOOP;
     }
     
     @Override
@@ -36,34 +61,32 @@ final class DefaultPooledByteBufferHolder implements PooledByteBufferHolder
     
     @Override
     public void release() {
-        IntConsumer f = onRelease.getAndSet(null);
-        if (f == null) {
+        final IntConsumer f = onRelease.getAndSet(RELEASED);
+        if (f == RELEASED) {
             return;
         }
         
+        if (buf != view) {
+            // Update source position
+            buf.position(buf.position() + view.position());
+        }
+        
+        final int read = thenRemaining - buf.remaining();
         try {
-            if (buf != view) {
-                // Update source position
-                buf.position(buf.position() + view.position());
-            }
-            int read = thenRemaining - buf.remaining();
             f.accept(read);
         } finally {
             buf = null;
             view = null;
+            afterRelease.accept(read);
         }
     }
     
     @Override
     public boolean onRelease(IntConsumer onRelease) {
         requireNonNull(onRelease);
-        
-        IntConsumer c1 = this.onRelease.get();
-        if (c1 == null) {
-            return false;
-        }
-        
-        return this.onRelease.compareAndSet(c1, c1.andThen(onRelease));
+        UnaryOperator<IntConsumer> keepReleasedOrAdd = v ->
+                v == RELEASED ? RELEASED : v.andThen(onRelease);
+        return this.onRelease.getAndUpdate(keepReleasedOrAdd) != RELEASED;
     }
     
     void limit(int newLimit) {
