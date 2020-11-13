695bf3518676c8c74d8e5945a438bcc3f214f5c1
Martin Andersson <webmaster@martinandersson.com>
2020-11-13 11:17:54 +0100

    Refactor

--- src/test/unit/java/alpha/nomagichttp/internal/DetailedEndToEndTest.java
+++ src/test/unit/java/alpha/nomagichttp/internal/DetailedEndToEndTest.java
@@ -1,12 +1,15 @@
 package alpha.nomagichttp.internal;
 
-import alpha.nomagichttp.handler.Handler;
+import alpha.nomagichttp.message.Request;
+import alpha.nomagichttp.message.Response;
 import alpha.nomagichttp.message.Responses;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
 
-import static alpha.nomagichttp.handler.Handlers.GET;
 import static alpha.nomagichttp.handler.Handlers.POST;
 import static alpha.nomagichttp.internal.ClientOperations.CRLF;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,50 +23,23 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class DetailedEndToEndTest extends AbstractEndToEndTest
 {
-    /**
-     * Performs two requests in a row.
-     */
-    @Test
-    void exchange_restart() throws IOException, InterruptedException {
-        // Echo request body as-is
-        Handler echo = POST().apply(req ->
-                req.body().get().toText().thenApply(Responses::ok));
+    private static final String
+            IS_BODY_EMPTY = "/is-body-empty",
+            ECHO_BODY     = "/echo-body";
+    
+    @BeforeAll
+    static void installHandlers() {
+        Function<Request, CompletionStage<Response>>
+                isBodyEmpty = req -> Responses.ok(String.valueOf(req.body().isEmpty())).asCompletedStage(),
+                echoBody    = req -> req.body().get().toText().thenApply(Responses::ok);
         
-        addHandler("/restart", echo);
-        
-        final String reqHead =
-            "POST /restart HTTP/1.1" + CRLF +
-            "Accept: text/plain; charset=utf-8" + CRLF +
-            "Content-Length: 3" + CRLF + CRLF;
-        
-        final String resHead =
-            "HTTP/1.1 200 OK" + CRLF +
-            "Content-Type: text/plain; charset=utf-8" + CRLF +
-            "Content-Length: 3" + CRLF + CRLF;
-        
-        client().openConnection();
-        
-        try {
-            String res1 = client().writeRead(reqHead + "ABC", "ABC");
-            assertThat(res1).isEqualTo(resHead + "ABC");
-            
-            String res2 = client().writeRead(reqHead + "DEF", "DEF");
-            assertThat(res2).isEqualTo(resHead + "DEF");
-        } finally {
-            client().closeConnection();
-        }
+        addHandler(IS_BODY_EMPTY, POST().apply(isBodyEmpty));
+        addHandler(ECHO_BODY,     POST().apply(echoBody));
     }
     
     @Test
-    void empty_body() throws IOException, InterruptedException {
-        Handler h = GET().apply(req -> Responses.ok(
-                  String.valueOf(req.body().isEmpty())
-                ).asCompletedStage());
-        
-        addHandler("/empty-body", h);
-        
-        String req = "GET /empty-body HTTP/1.1" + CRLF + CRLF,
-               res = client().writeRead(req, "true");
+    void empty_request_body() throws IOException, InterruptedException {
+        String res = client().writeRead(requestWithBody(IS_BODY_EMPTY, ""), "true");
         
         assertThat(res).isEqualTo(
             "HTTP/1.1 200 OK" + CRLF +
@@ -73,6 +49,30 @@ class DetailedEndToEndTest extends AbstractEndToEndTest
             "true");
     }
     
-    // TODO: Autodiscard request body test. Handler should be able to respond without consuming body.
-    //       Handler should be able to echo half a body, byte by byte, and full body, byte by byte
+    @Test
+    void connection_reuse() throws IOException, InterruptedException {
+        final String resHead =
+            "HTTP/1.1 200 OK" + CRLF +
+            "Content-Type: text/plain; charset=utf-8" + CRLF +
+            "Content-Length: 3" + CRLF + CRLF;
+        
+        client().openConnection();
+        
+        try {
+            String res1 = client().writeRead(requestWithBody(ECHO_BODY, "ABC"), "ABC");
+            assertThat(res1).isEqualTo(resHead + "ABC");
+            
+            String res2 = client().writeRead(requestWithBody(ECHO_BODY, "DEF"), "DEF");
+            assertThat(res2).isEqualTo(resHead + "DEF");
+        } finally {
+            client().closeConnection();
+        }
+    }
+    
+    private static String requestWithBody(String path, String body) {
+        return "POST " + path + " HTTP/1.1" + CRLF +
+               "Accept: text/plain; charset=utf-8" + CRLF +
+               "Content-Length: " + body.length() + CRLF + CRLF +
+               body;
+    }
 }
