08c764b28e7c989fa9fe8a3d9ab2decff9be0aec
Martin Andersson <webmaster@martinandersson.com>
2020-11-21 23:04:19 +0100

    Fix missing import

--- src/test/unit/java/alpha/nomagichttp/internal/DetailedEndToEndTest.java
+++ src/test/unit/java/alpha/nomagichttp/internal/DetailedEndToEndTest.java
@@ -14,6 +14,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channel;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Flow;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static alpha.nomagichttp.handler.Handlers.POST;
