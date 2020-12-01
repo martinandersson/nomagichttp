d30b3c4095f0dadb333b62a9edeb3656fc904db1
Martin Andersson <webmaster@martinandersson.com>
2020-12-01 19:33:10 +0100

    Refactor CollectingSubscriber long-typed demand to a Request strategy

--- src/test/unit/java/alpha/nomagichttp/util/CollectingSubscriber.java
+++ src/test/unit/java/alpha/nomagichttp/util/CollectingSubscriber.java
@@ -6,13 +6,40 @@ import java.util.concurrent.Flow;
 import java.util.stream.Collectors;
 
 import static java.lang.Long.MAX_VALUE;
+import static java.util.stream.Collectors.toList;
 
-final class CollectingSubscriber<T> implements Flow.Subscriber<T>
+class CollectingSubscriber<T> implements Flow.Subscriber<T>
 {
     static <T> Collection<T> drain(Flow.Publisher<? extends T> publisher) {
-        CollectingSubscriber<T> s = new CollectingSubscriber<>();
+        CollectingSubscriber<T> s = new CollectingSubscriber<>(Request.IMMEDIATELY_MAX());
         publisher.subscribe(s);
         return s.items();
+    }
+    
+    static class Request {
+        private static final Request NOTHING = new Request(-1);
+        
+        static Request NOTHING() {
+            return NOTHING;
+        }
+        
+        static Request IMMEDIATELY_N(long value) {
+            return new Request(value);
+        }
+        
+        static Request IMMEDIATELY_MAX() {
+            return new Request(MAX_VALUE);
+        }
+        
+        private final long value;
+        
+        private Request(long value) {
+            this.value = value;
+        }
+        
+        long value() {
+            return value;
+        }
     }
     
     static abstract class Signal {
@@ -52,28 +79,30 @@ final class CollectingSubscriber<T> implements Flow.Subscriber<T>
     }
     
     private final Collection<Signal> signals;
-    private final long request;
+    private final Request strategy;
     
-    CollectingSubscriber() {
-        this(MAX_VALUE);
+    CollectingSubscriber(Request strategy) {
+        this.signals = new ConcurrentLinkedQueue<>();
+        this.strategy = strategy;
     }
     
-    CollectingSubscriber(long request) {
-        this.signals = new ConcurrentLinkedQueue<>();
-        this.request = request;
+    Collection<Class<?>> signals() {
+        return signals.stream().map(Signal::getClass)
+                               .collect(toList());
     }
     
     Collection<T> items() {
-        return signals.stream()
-                .filter(s -> s instanceof Signal.Next)
-                .map(s -> ((Signal.Next) s).<T>item())
-                .collect(Collectors.toUnmodifiableList());
+        return signals.stream().filter(s -> s instanceof Signal.Next)
+                               .map(s -> ((Signal.Next) s).<T>item())
+                               .collect(Collectors.toUnmodifiableList());
     }
     
     @Override
     public void onSubscribe(Flow.Subscription subscription) {
         signals.add(new Signal.Subscribe());
-        subscription.request(request);
+        if (strategy != Request.NOTHING) {
+            subscription.request(strategy.value());
+        }
     }
     
     @Override
