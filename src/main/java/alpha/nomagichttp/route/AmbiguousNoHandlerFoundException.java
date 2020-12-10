45f59795fdae643701139606fbebc4a3f93b6a4b
Martin Andersson <webmaster@martinandersson.com>
2020-12-10 18:18:57 +0100

    Add JavaDoc

--- src/main/java/alpha/nomagichttp/route/AmbiguousNoHandlerFoundException.java
+++ src/main/java/alpha/nomagichttp/route/AmbiguousNoHandlerFoundException.java
@@ -1,5 +1,6 @@
 package alpha.nomagichttp.route;
 
+import alpha.nomagichttp.handler.ErrorHandler;
 import alpha.nomagichttp.handler.RequestHandler;
 
 import java.util.Set;
@@ -7,8 +8,14 @@ import java.util.Set;
 import static java.util.Collections.unmodifiableSet;
 import static java.util.Objects.requireNonNull;
 
-// TODO: Document
 public final class AmbiguousNoHandlerFoundException extends RuntimeException {
+/**
+ * Thrown by the HTTP server if the {@link RequestHandler} resolution process
+ * ends ambiguously. The response produced by {@link ErrorHandler#DEFAULT}
+ * is "501 Not Implemented".
+ * 
+ * @author Martin Andersson (webmaster at martinandersson.com)
+ */
     private final Set<RequestHandler> ambiguous;
     
     AmbiguousNoHandlerFoundException(Set<RequestHandler> ambiguous, String message) {
