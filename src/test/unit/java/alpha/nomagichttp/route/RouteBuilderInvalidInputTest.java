e343dc380923af3b0c704cf255b953f1cb818c92
Martin Andersson <webmaster@martinandersson.com>
2020-12-14 18:02:02 +0100

    Remove duplicated test coverage

--- src/test/unit/java/alpha/nomagichttp/route/RouteBuilderInvalidInputTest.java
+++ src/test/unit/java/alpha/nomagichttp/route/RouteBuilderInvalidInputTest.java
@@ -23,37 +23,12 @@ class RouteBuilderInvalidInputTest
     }
     
     @Test
-    void segment_can_not_be_empty_concat_1() {
-        Route.Builder rb = builder("/");
-        
-        assertThatThrownBy(() -> rb.append(""))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment must start with a forward slash character.");
-    }
-    
-    @Test
-    void segment_can_not_be_empty_concat_2() {
+    void segment_can_not_be_empty_concat() {
         Route.Builder rb = builder("/");
         
         assertThatThrownBy(() -> rb.append("/"))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Segment must contain more than just a forward slash character.");
-    }
-    
-    @Test
-    void segment_must_start_with_slash_ctor() {
-        assertThatThrownBy(() -> builder("x"))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment must start with a forward slash character.");
-    }
-    
-    @Test
-    void segment_must_start_with_slash_concat() {
-        Route.Builder rb = builder("/");
-        
-        assertThatThrownBy(() -> rb.append("x"))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment must start with a forward slash character.");
     }
     
     @Test
@@ -73,33 +48,8 @@ class RouteBuilderInvalidInputTest
     }
     
     @Test
-    void segment_can_not_have_two_trailing_slashes_ctor() {
-        assertThatThrownBy(() -> builder("/a//"))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment is empty.");
-    }
-    
-    @Test
-    void segment_can_not_have_two_trailing_slashes_concat() {
-        Route.Builder rb = builder("/");
-    
-        assertThatThrownBy(() -> rb.append("/a//"))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment must not end with a forward slash character.");
-    }
-    
-    @Test
-    void segment_can_not_be_null_ctor() {
+    void segment_can_not_be_null() {
         assertThatThrownBy(() -> builder(null))
-                .isExactlyInstanceOf(NullPointerException.class)
-                .hasMessage(null);
-    }
-    
-    @Test
-    void segment_can_not_be_null_concat() {
-        Route.Builder rb = builder("/");
-        
-        assertThatThrownBy(() -> rb.append(null))
                 .isExactlyInstanceOf(NullPointerException.class)
                 .hasMessage(null);
     }
