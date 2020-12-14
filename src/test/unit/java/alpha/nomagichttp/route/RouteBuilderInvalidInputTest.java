037ec2044fd645c75e7082dcdeb7abdb02ffbdec
Martin Andersson <webmaster@martinandersson.com>
2020-12-14 18:13:03 +0100

    Reduce noise

--- src/test/unit/java/alpha/nomagichttp/route/RouteBuilderInvalidInputTest.java
+++ src/test/unit/java/alpha/nomagichttp/route/RouteBuilderInvalidInputTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static alpha.nomagichttp.route.Route.builder;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 
 /**
  * Provokes exceptions from {@code Route.Builder} caused by invalid segment- and
@@ -17,58 +18,56 @@ class RouteBuilderInvalidInputTest
 {
     @Test
     void segment_can_not_be_empty_ctor() {
-        assertThatThrownBy(() -> Route.builder(""))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment must start with a forward slash character.");
+        assertThrows(() -> Route.builder(""),
+                IllegalArgumentException.class,
+                "Segment must start with a forward slash character.");
     }
     
     @Test
     void segment_can_not_be_empty_concat() {
         Route.Builder rb = builder("/");
-        
-        assertThatThrownBy(() -> rb.append("/"))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment must contain more than just a forward slash character.");
+        assertThrows(() -> rb.append("/"),
+                IllegalArgumentException.class,
+                "Segment must contain more than just a forward slash character.");
     }
     
     @Test
     void segment_can_not_be_two_slashes_ctor() {
-        assertThatThrownBy(() -> builder("//"))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment is empty.");
+        assertThrows(() -> builder("//"),
+                IllegalArgumentException.class,
+                "Segment is empty.");
     }
     
     @Test
     void segment_can_not_be_two_slashes_concat() {
         Route.Builder rb = builder("/");
-        
-        assertThatThrownBy(() -> rb.append("//"))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Segment must not end with a forward slash character.");
+        assertThrows(() -> rb.append("//"),
+                IllegalArgumentException.class,
+                "Segment must not end with a forward slash character.");
     }
     
     @Test
     void segment_can_not_be_null() {
-        assertThatThrownBy(() -> builder(null))
-                .isExactlyInstanceOf(NullPointerException.class)
-                .hasMessage(null);
+        assertThrows(() -> builder(null), NullPointerException.class, null);
     }
     
     @Test
     void param_can_not_be_null() {
         Route.Builder rb = builder("/");
-        
-        assertThatThrownBy(() -> rb.param(null))
-                .isExactlyInstanceOf(NullPointerException.class)
-                .hasMessage(null);
+        assertThrows(() -> rb.param(null), NullPointerException.class, null);
     }
     
     @Test
     void param_name_must_be_unique() {
         Route.Builder rb = builder("/").param("name");
-        
-        assertThatThrownBy(() -> rb.param("name"))
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Duplicate parameter name: \"name\"");
+        assertThrows(() -> rb.param("name"),
+                IllegalArgumentException.class,
+                "Duplicate parameter name: \"name\"");
+    }
+    
+    private static void assertThrows(ThrowingCallable code, Class<?> exactlyInstanceOf, String hasMessage) {
+        assertThatThrownBy(code)
+                .isExactlyInstanceOf(exactlyInstanceOf)
+                .hasMessage(hasMessage);
     }
 }
