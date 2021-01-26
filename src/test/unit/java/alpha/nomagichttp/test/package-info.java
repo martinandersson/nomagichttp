5a32046ef8ab98946ca547581d52ecd343319f11
Martin Andersson <webmaster@martinandersson.com>
2021-01-26 13:41:49 +0100

    Add package-info

---
+++ src/test/unit/java/alpha/nomagichttp/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package is the location of top-level utils specific to small tests. This
+ * includes utils even if they are only used by one other package. Other
+ * packages contain test classes and only test classes. The purpose is to make
+ * it easier for developers to locate test files vs. utilities.
+ */
+package alpha.nomagichttp.test;
