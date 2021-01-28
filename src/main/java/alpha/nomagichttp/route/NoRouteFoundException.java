b2d62fa5f45cda44e6a68e9a2c85255a6566886f
Martin Andersson <webmaster@martinandersson.com>
2021-01-28 15:27:26 +0100

    Add NoRouteFoundException.segments()

--- src/main/java/alpha/nomagichttp/route/NoRouteFoundException.java
+++ src/main/java/alpha/nomagichttp/route/NoRouteFoundException.java
@@ -1,19 +1,39 @@
 package alpha.nomagichttp.route;
 
+import alpha.nomagichttp.handler.ErrorHandler;
+
+import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.StreamSupport.stream;
 
 /**
- * TODO: Docs
+ * Thrown by a route registry that failed to lookup a route because the route
+ * was not added. {@link ErrorHandler#DEFAULT} maps this exception to a "404 Not
+ * Found" response.
  * 
  * @see RouteRegistry#lookup(Iterable) 
+ * 
+ * @author Martin Andersson (webmaster at martinandersson.com)
  */
 public class NoRouteFoundException extends RuntimeException
 {
-    private final String path;
+    private final Iterable<String> segments;
     
     public NoRouteFoundException(Iterable<String> pathSegments) {
-        this.path = "/" + stream(pathSegments.spliterator(), false).collect(joining("/"));
+        this.segments = requireNonNull(pathSegments);
+    }
+    
+    /**
+     * Returns normalized- and percent-decoded path segments as provided in the
+     * request target.
+     * 
+     * @return normalized- and percent-decoded path segments as provided in the
+     *         request target
+     * 
+     * @see Route
+     */
+    public Iterable<String> segments() {
+        return segments;
     }
     
     /**
@@ -23,6 +43,6 @@ public class NoRouteFoundException extends RuntimeException
      *         (never {@code null} or the empty string)
      */
     public String path() {
-        return path;
+        return "/" + stream(segments().spliterator(), false).collect(joining("/"));
     }
 }
