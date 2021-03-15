985dc7cfddc006820c80821fff18038fb43fa006
Martin Andersson <webmaster@martinandersson.com>
2021-03-15 15:22:50 +0100

    Clean-up

--- src/test/unit/java/alpha/nomagichttp/internal/ErrorHandlingTest.java
+++ src/test/unit/java/alpha/nomagichttp/internal/ErrorHandlingTest.java
@@ -18,8 +18,7 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
-import static alpha.nomagichttp.HttpServer.Config.DEFAULT;
-import static alpha.nomagichttp.handler.RequestHandlers.noop;
+import static alpha.nomagichttp.HttpServer.create;
 import static alpha.nomagichttp.testutil.ClientOperations.CRLF;
 import static java.lang.System.Logger.Level.ALL;
 import static java.util.concurrent.CompletableFuture.failedFuture;
@@ -32,11 +31,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class ErrorHandlingTest
 {
-    private static final String
-            REQ_ROOT      = "GET / HTTP/1.1"    + CRLF + CRLF,
-            REQ_NOT_FOUND = "GET /404 HTTP/1.1" + CRLF + CRLF;
-    
-    HttpServer server;
+    HttpServer s;
     
     @BeforeAll
     static void setLogging() {
@@ -45,34 +40,40 @@ class ErrorHandlingTest
     
     @AfterEach
     void stopServer() throws IOException {
-        if (server != null) {
-            server.stop();
+        if (s != null) {
+            s.stop();
         }
     }
     
     @Test
     void not_found_default() throws IOException {
-        String res = createServerAndClient().writeRead(REQ_NOT_FOUND);
-        assertThat(res).isEqualTo(
+        s = create().start();
+        String r = new ClientOperations(s).writeRead(
+            "GET /404 HTTP/1.1" + CRLF + CRLF);
+        
+        assertThat(r).isEqualTo(
             "HTTP/1.1 404 Not Found" + CRLF +
-            "Content-Length: 0" + CRLF + CRLF);
+            "Content-Length: 0"      + CRLF + CRLF);
     }
     
     @Test
     void not_found_custom() throws IOException {
-        ErrorHandler custom = (exc, req, han) -> {
+        ErrorHandler eh = (exc, req, han) -> {
             if (exc instanceof NoRouteFoundException) {
                 return Response.builder()
-                        .statusCode(123)
-                        .reasonPhrase("Custom Not Found!")
-                        .mustCloseAfterWrite(true)
-                        .build()
-                        .completedStage();
+                               .statusCode(123)
+                               .reasonPhrase("Custom Not Found!")
+                               .mustCloseAfterWrite(true)
+                               .build()
+                               .completedStage();
             }
             throw exc;
         };
         
-        String res = createServerAndClient(custom).writeRead(REQ_NOT_FOUND);
+        s = create(eh).start();
+        String res = new ClientOperations(s).writeRead(
+            "GET /404 HTTP/1.1" + CRLF + CRLF);
+        
         assertThat(res).isEqualTo(
             "HTTP/1.1 123 Custom Not Found!" + CRLF + CRLF);
     }
@@ -100,27 +101,33 @@ class ErrorHandlingTest
             }
             
             return Response.Builder.ok()
-                    .header("N", Integer.toString(c.get()))
-                    .addHeader("Content-Length", "0")
-                    .build()
-                    .completedStage();
+                                   .header("N", Integer.toString(c.get()))
+                                   .addHeader("Content-Length", "0")
+                                   .build()
+                                   .completedStage();
         });
         
         ErrorHandler retry = (t, r, h2) -> h2.logic().apply(r);
         
-        String res = createServerAndClient(h1, retry, null).writeRead(REQ_ROOT);
-        assertThat(res).isEqualTo(
-            "HTTP/1.1 200 OK" + CRLF +
-            "N: 3" + CRLF +
+        s = create(retry).add("/", h1).start();;
+        String r = new ClientOperations(s).writeRead(
+            "GET / HTTP/1.1" + CRLF + CRLF);
+        
+        assertThat(r).isEqualTo(
+            "HTTP/1.1 200 OK"   + CRLF +
+            "N: 3"              + CRLF +
             "Content-Length: 0" + CRLF + CRLF);
     }
     
     @Test
     void httpVersionBad() throws IOException {
-        String res = createServerAndClient().writeRead("GET / Ooops" + CRLF + CRLF);
+        s = create().start();;
+        String res = new ClientOperations(s).writeRead(
+            "GET / Ooops" + CRLF + CRLF);
+        
         assertThat(res).isEqualTo(
             "HTTP/1.1 400 Bad Request" + CRLF +
-            "Content-Length: 0" + CRLF + CRLF);
+            "Content-Length: 0"        + CRLF + CRLF);
     }
     
     /**
@@ -130,16 +137,18 @@ class ErrorHandlingTest
      */
     @Test
     void httpVersionRejected_tooOld_byDefault() throws IOException {
-        ClientOperations c = createServerAndClient();
+        s = create().start();;
+        ClientOperations c = new ClientOperations(s);
         
         for (String v : List.of("-1.23", "0.5", "0.8", "0.9")) {
-            String res = c.writeRead("GET / HTTP/" + v + CRLF + CRLF);
-            
-            assertThat(res).isEqualTo(
-                "HTTP/1.1 426 Upgrade Required" + CRLF +
-                "Upgrade: HTTP/1.1" + CRLF +
-                "Connection: Upgrade" + CRLF +
-                "Content-Length: 0" + CRLF + CRLF);
+             String res = c.writeRead(
+                 "GET / HTTP/" + v + CRLF + CRLF);
+             
+             assertThat(res).isEqualTo(
+                 "HTTP/1.1 426 Upgrade Required" + CRLF +
+                 "Upgrade: HTTP/1.1"             + CRLF +
+                 "Connection: Upgrade"           + CRLF +
+                 "Content-Length: 0"             + CRLF + CRLF);
         }
     }
     
@@ -158,10 +167,11 @@ class ErrorHandlingTest
             }
         };
         
-        ClientOperations c = createServerAndClient(noop(), null, rejectHttp1_0);
-        String res = c.writeRead("GET /not-found HTTP/1.0" + CRLF + CRLF);
+        s = create(rejectHttp1_0).start();;
+        String r = new ClientOperations(s).writeRead(
+            "GET /not-found HTTP/1.0" + CRLF + CRLF);
         
-        assertThat(res).isEqualTo(
+        assertThat(r).isEqualTo(
             "HTTP/1.0 426 Upgrade Required" + CRLF +
             "Upgrade: HTTP/1.1"             + CRLF +
             "Connection: Upgrade"           + CRLF +
@@ -170,33 +180,16 @@ class ErrorHandlingTest
     
     @Test
     void httpVersionRejected_tooNew() throws IOException {
-        ClientOperations c = createServerAndClient();
+        s = create().start();;
+        ClientOperations c = new ClientOperations(s);
         
         for (String v : List.of("2", "3", "999")) {
-            String res = c.writeRead("GET / HTTP/" + v + CRLF + CRLF);
-            
-            assertThat(res).isEqualTo(
-                "HTTP/1.1 505 HTTP Version Not Supported" + CRLF +
-                "Content-Length: 0" + CRLF + CRLF);
+             String r = c.writeRead(
+                 "GET / HTTP/" + v + CRLF + CRLF);
+             
+             assertThat(r).isEqualTo(
+                 "HTTP/1.1 505 HTTP Version Not Supported" + CRLF +
+                 "Content-Length: 0" + CRLF + CRLF);
         }
-    }
-    
-    private ClientOperations createServerAndClient() throws IOException {
-        return createServerAndClient(null);
-    }
-    
-    private ClientOperations createServerAndClient(ErrorHandler onError) throws IOException {
-        return createServerAndClient(noop(), onError, null);
-    }
-    
-    private ClientOperations createServerAndClient(RequestHandler handler, ErrorHandler onError, HttpServer.Config cfg) throws IOException {
-        ErrorHandler[] eh = onError == null ?
-                new ErrorHandler[0] :
-                new ErrorHandler[]{ onError };
-        
-        HttpServer.Config c = cfg == null ? DEFAULT : cfg;
-        
-        server = HttpServer.create(c, eh).add("/", handler).start();
-        return new ClientOperations(server);
     }
 }
