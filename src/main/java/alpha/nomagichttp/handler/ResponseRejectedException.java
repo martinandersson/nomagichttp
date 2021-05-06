5382f42fd6b6de91a29b19d911fbaa84ffd4579c
Martin Andersson <webmaster@martinandersson.com>
2021-05-06 20:51:08 +0200

    Remove obsolete comment

--- src/main/java/alpha/nomagichttp/handler/ResponseRejectedException.java
+++ src/main/java/alpha/nomagichttp/handler/ResponseRejectedException.java
@@ -20,8 +20,6 @@ public class ResponseRejectedException extends RuntimeException
      * The reason why a response was rejected.
      */
     public enum Reason {
-        // TODO: Also need to "close" exchange on channel close.
-        // TODO: Also need to "close" exchange on channel close in this branch
         /**
          * The response status-code is 1XX and HTTP version used is {@literal <} 1.1.
          */
