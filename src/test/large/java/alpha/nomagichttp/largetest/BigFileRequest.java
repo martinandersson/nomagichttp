c33232b0bbf84d700ef85cd84f261f9e9fbdc5ba
Martin Andersson <webmaster@martinandersson.com>
2021-05-15 16:09:09 +0200

    Add test for retrieving file

--- src/test/large/java/alpha/nomagichttp/largetest/BigFileRequest.java
+++ src/test/large/java/alpha/nomagichttp/largetest/BigFileRequest.java
@@ -1,53 +1,75 @@
 package alpha.nomagichttp.largetest;
 
 import alpha.nomagichttp.handler.RequestHandler;
-import org.junit.jupiter.api.BeforeAll;
+import alpha.nomagichttp.message.Response;
+import alpha.nomagichttp.message.Responses;
+import alpha.nomagichttp.util.BetterBodyPublishers;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static alpha.nomagichttp.handler.RequestHandler.GET;
 import static alpha.nomagichttp.handler.RequestHandler.POST;
 import static alpha.nomagichttp.message.Responses.noContent;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 /**
- * POSTs a big file (50 MB) to server and verifies all the disk contents.
+ * POST a big file (50 MB) to server, verify disk contents, respond same file.
  * 
  * @author Martin Andersson (webmaster at martinandersson.com)
  */
+@TestMethodOrder(OrderAnnotation.class)
 class BigFileRequest extends AbstractSingleClientTest
 {
-    private static Path FILE;
+    private static Path file;
+    private static byte[] contents;
+    private static boolean saved;
     
-    @BeforeAll
-    static void beforeAll() throws IOException {
-        FILE = Files.createTempDirectory("nomagic")
-                    .resolve("50MB");
+    @Test
+    @Order(1)
+    void post() throws IOException, InterruptedException {
+        file = Files.createTempDirectory("nomagic").resolve("50MB");
         
-        RequestHandler fileSaver = POST().apply(req ->
+        RequestHandler rh = POST().apply(req ->
                 req.body()
-                   .toFile(FILE)
+                   .toFile(file)
                    .thenApply(n ->
-                       noContent()
-                       .toBuilder()
-                       .header("Received", Long.toString(n))
-                       .build()));
+                       noContent().toBuilder()
+                                  .header("Received", Long.toString(n))
+                                  .build()));
         
-        server().add("/file", fileSaver);
+        server().add("/receive", rh);
+        
+        int len = 50 * 1_000_000;
+        contents = DataUtil.bytes(len);
+        HttpResponse<Void> res = postBytes("/receive", contents);
+        
+        assertThat(res.statusCode()).isEqualTo(204);
+        assertThat(res.headers().firstValue("Received")).contains(Long.toString(len));
+        assertThat(Files.readAllBytes(file)).isEqualTo(contents);
+        saved = true;
     }
     
     @Test
-    void test() throws IOException, InterruptedException {
-        final int len = 50 * 1_000_000;
+    @Order(2)
+    void get() throws IOException, InterruptedException {
+        assumeTrue(saved);
         
-        byte[] expected = DataUtil.bytes(len);
+        Response r = Responses.ok(BetterBodyPublishers.ofFile(file));
+        RequestHandler rh = GET().respond(r);
+        server().add("/retrieve", rh);
         
-        HttpResponse<Void> res = postBytes("/file", expected);
-        assertThat(res.statusCode()).isEqualTo(204);
-        assertThat(res.headers().firstValue("Received")).contains(Long.toString(len));
-        assertThat(Files.readAllBytes(FILE)).isEqualTo(expected);
+        HttpResponse<byte[]> res = getBytes("/retrieve");
+        
+        assertThat(res.statusCode()).isEqualTo(200);
+        assertThat(res.headers().firstValue("Content-Type")).contains("application/octet-stream");
+        assertThat(res.body()).isEqualTo(contents);
     }
 }
