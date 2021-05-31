f5e9bf859524b387144e4a96f4a9c1e389414482
Martin Andersson <webmaster@martinandersson.com>
2021-05-31 14:58:31 +0200

    Add logging

--- src/test/unit/java/alpha/nomagichttp/testutil/Interrupt.java
+++ src/test/unit/java/alpha/nomagichttp/testutil/Interrupt.java
@@ -7,13 +7,21 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.System.Logger.Level.DEBUG;
+
 /**
- * Interrupt current thread after a given amount of time.
+ * Interrupt current thread after a given amount of time.<p>
+ * 
+ * If the specified {@code duration} is zero or negative, the calling thread
+ * will be interrupted immediately and enter the action in an interrupted state.
+ * Otherwise, the interrupt is done by a background thread.
  * 
  * @author Martin Andersson (webmaster at martinandersson.com)
  */
 public class Interrupt
 {
+    private static final System.Logger LOG = System.getLogger(Interrupt.class.getPackageName());
+    
     /**
      * Executes the given action and interrupts the current thread after a
      * specified amount of time, unless the action completes sooner.
@@ -35,11 +43,7 @@ public class Interrupt
     /**
      * Retrieves the result of the given action and interrupts the current
      * thread after a specified amount of time, unless the action completes
-     * sooner.<p>
-     * 
-     * If {@code duration} is zero or negative, the calling thread will be
-     * interrupted immediately and enter the action in an interrupted state.
-     * Otherwise, the interrupt is done by a background thread.
+     * sooner.
      * 
      * @param duration duration of timeout
      * @param unit unit of duration
@@ -63,6 +67,7 @@ public class Interrupt
         ScheduledFuture<?> task = SCHEDULER.schedule(() -> {
             synchronized (timer) {
                 if (timer[0]) {
+                    LOG.log(DEBUG, "Interrupting.");
                     worker.interrupt();
                     timer[0] = false;
                 }
