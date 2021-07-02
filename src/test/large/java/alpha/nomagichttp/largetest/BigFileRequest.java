221b1a9df46dc27c78d9fd832682cb08969570d5
Martin Andersson <webmaster@martinandersson.com>
2021-07-02 17:59:14 +0200

    Switch superclass

--- src/test/large/java/alpha/nomagichttp/largetest/BigFileRequest.java
+++ src/test/large/java/alpha/nomagichttp/largetest/BigFileRequest.java
@@ -1,22 +1,22 @@
 package alpha.nomagichttp.largetest;
 
-import alpha.nomagichttp.handler.RequestHandler;
-import alpha.nomagichttp.message.Response;
-import alpha.nomagichttp.message.Responses;
-import alpha.nomagichttp.util.BetterBodyPublishers;
+import alpha.nomagichttp.testutil.AbstractRealTest;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import java.io.IOException;
-import java.net.http.HttpResponse;
+import java.nio.channels.Channel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static alpha.nomagichttp.handler.RequestHandler.GET;
 import static alpha.nomagichttp.handler.RequestHandler.POST;
 import static alpha.nomagichttp.message.Responses.noContent;
+import static alpha.nomagichttp.message.Responses.ok;
+import static alpha.nomagichttp.testutil.TestClient.CRLF;
+import static alpha.nomagichttp.util.BetterBodyPublishers.ofFile;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
  * @author Martin Andersson (webmaster at martinandersson.com)
  */
 @TestMethodOrder(OrderAnnotation.class)
-class BigFileRequest extends AbstractSingleClientTest
+class BigFileRequest extends AbstractRealTest
 {
     private static Path file;
     private static byte[] contents;
@@ -34,42 +34,59 @@ class BigFileRequest extends AbstractSingleClientTest
     
     @Test
     @Order(1)
-    void post() throws IOException, InterruptedException {
+    void post() throws IOException {
         file = Files.createTempDirectory("nomagic").resolve("50MB");
         
-        RequestHandler rh = POST().apply(req ->
-                req.body()
-                   .toFile(file)
-                   .thenApply(n ->
-                       noContent().toBuilder()
-                                  .header("Received", Long.toString(n))
-                                  .build()));
-        
-        server().add("/receive", rh);
+        server().add("/file", POST().apply(req ->
+                req.body().toFile(file).thenApply(len ->
+                        noContent().toBuilder()
+                                   .header("Received", Long.toString(len))
+                                   .build())));
         
         int len = 50 * 1_000_000;
         contents = DataUtil.bytes(len);
-        HttpResponse<Void> res = postBytes("/receive", contents);
         
-        assertThat(res.statusCode()).isEqualTo(204);
-        assertThat(res.headers().firstValue("Received")).contains(Long.toString(len));
+        final String rsp;
+        Channel conn = client().openConnection();
+        try (conn) {
+            rsp = client()
+                      .write(
+                          "POST /file HTTP/1.1"     + CRLF +
+                          "Content-Length: " + len  + CRLF +
+                          "Connection: close"       + CRLF + CRLF)
+                      .write(
+                          contents)
+                      .shutdownOutput()
+                      .readTextUntilEOS();
+        }
+        
+        assertThat(rsp).isEqualTo(
+            "HTTP/1.1 204 No Content" + CRLF +
+            "Received: " + len        + CRLF +
+            "Connection: close"       + CRLF + CRLF);
         assertThat(Files.readAllBytes(file)).isEqualTo(contents);
         saved = true;
     }
     
     @Test
     @Order(2)
-    void get() throws IOException, InterruptedException {
+    void get() throws IOException {
         assumeTrue(saved);
+        server().add("/file", GET().respond(ok(ofFile(file))));
         
-        Response r = Responses.ok(BetterBodyPublishers.ofFile(file));
-        RequestHandler rh = GET().respond(r);
-        server().add("/retrieve", rh);
-        
-        HttpResponse<byte[]> res = getBytes("/retrieve");
-        
-        assertThat(res.statusCode()).isEqualTo(200);
-        assertThat(res.headers().firstValue("Content-Type")).contains("application/octet-stream");
-        assertThat(res.body()).isEqualTo(contents);
+        final byte[] body;
+        Channel conn = client().openConnection();
+        try (conn) {
+            assertThat(client().writeReadTextUntilNewlines(
+                "GET /file HTTP/1.1"                     + CRLF +
+                "Connection: close"                      + CRLF + CRLF))
+                    .isEqualTo(
+                "HTTP/1.1 200 OK"                        + CRLF +
+                "Content-Type: application/octet-stream" + CRLF +
+                "Content-Length: " + contents.length     + CRLF +
+                "Connection: close"                      + CRLF + CRLF);
+            body = client().readBytesUntilEOS();
+        }
+        assertThat(body).isEqualTo(contents);
     }
 }
