3d41913d6a4bd5475eac4d9300e530d7e40b06d3
Martin Andersson <webmaster@martinandersson.com>
2021-08-19 10:09:08 +0200

    Extract AbstractStats

--- src/main/java/alpha/nomagichttp/events/ResponseSent.java
+++ src/main/java/alpha/nomagichttp/events/ResponseSent.java
@@ -2,18 +2,13 @@ package alpha.nomagichttp.events;
 
 import alpha.nomagichttp.message.Response;
 
-import java.time.Duration;
-import java.util.Objects;
-
-import static java.util.concurrent.TimeUnit.NANOSECONDS;
-
 /**
  * A response has been successfully sent.<p>
  * 
- * The intended purpose for this event is to gather metrics.<p>
+ * The intended purpose of this event is to gather metrics.<p>
  * 
  * The first attachment given to the listener is the {@link Response} sent. The
- * second attachment will be an instance of {@link Stats}.
+ * second attachment is an instance of {@link Stats}.
  * 
  * @author Martin Andersson (webmaster at martinandersson.com)
  */
@@ -24,21 +19,17 @@ public enum ResponseSent {
     INSTANCE;
     
     /**
-     * A thread-safe container of statistics concerning a response sent.<p>
+     * A container of statistics concerning a response sent.<p>
      * 
-     * This class contains no references to wall-clock time. The {@link
-     * ResponseSent} event is emitted immediately after {@link
-     * #nanoTimeOnStop()}, and so, a wall-clock reference to when the response
-     * begun transmission can be approximated:
-     * <pre>
-     *   var then = Instant.now().minusNanos({@link #elapsedNanos()});
-     * </pre>
-     * 
-     * This class implements {@code equals} and {@code hashCode}.
+     * {@link System#nanoTime()} is first pulled just before the first channel
+     * operation is initialized and last pulled after the last channel operation
+     * completes, just before the emission of the event. Time elapsed includes
+     * not only time that the response spent on the wire, but also time spent
+     * waiting for new body bytebuffers to be published.
      */
-    public static class Stats
+    public static final class Stats extends AbstractStats
     {
-        private final long start, stop, bytes;
+        private final long bytes;
         
         /**
          * Constructs this object.
@@ -48,70 +39,8 @@ public enum ResponseSent {
          * @param bytes transferred
          */
         public Stats(long start, long stop, long bytes) {
-            this.start = start;
-            this.stop  = stop;
+            super(start, stop);
             this.bytes = bytes;
-        }
-        
-        /**
-         * Returns the value from {@link System#nanoTime()} polled just before
-         * the first channel operation was initialized.
-         * 
-         * @return the value from {@link System#nanoTime()} polled just before
-         *         the first channel operation was initialized
-         */
-        public long nanoTimeOnStart() {
-            return start;
-        }
-        
-        /**
-         * Returns the value from {@link System#nanoTime()} polled just after
-         * the last channel operation completed.
-         * 
-         * @return the value from {@link System#nanoTime()} polled just after
-         * the last channel operation completed
-         */
-        public long nanoTimeOnStop() {
-            return stop;
-        }
-        
-        /**
-         * Returns nanos elapsed between {@link #nanoTimeOnStart()} and {@link
-         * #nanoTimeOnStop()}.<p>
-         * 
-         * This is the time it took for the response to be sent out and includes
-         * not only time spent on the wire, but also time spent waiting for new
-         * body bytebuffers to be published.
-         * 
-         * @return nanos elapsed between {@link #nanoTimeOnStart()} and {@link
-         *         #nanoTimeOnStop()}
-         */
-        public long elapsedNanos() {
-            return nanoTimeOnStop() - nanoTimeOnStart();
-        }
-        
-        /**
-         * Returns milliseconds elapsed between {@link #nanoTimeOnStart()} and
-         * {@link #nanoTimeOnStop()}.<p>
-         * 
-         * This is the time it took for the response to be sent out and includes
-         * not only time spent on the wire, but also time spent waiting for new
-         * body bytebuffers to be published.
-         * 
-         * @return milliseconds elapsed between {@link #nanoTimeOnStart()} and
-         *         {@link #nanoTimeOnStop()}
-         */
-        public long elapsedMillis() {
-            return NANOSECONDS.toMillis(elapsedNanos());
-        }
-        
-        /**
-         * Returns a {@code Duration} of {@link #elapsedNanos()}.
-         * 
-         * @return a {@code Duration} of {@link #elapsedNanos()}
-         */
-        public Duration elapsedDuration() {
-            return Duration.ofNanos(elapsedNanos());
         }
         
         /**
@@ -131,7 +60,7 @@ public enum ResponseSent {
             // Copy-paste from String.hashCode()
             int h = hash;
             if (h == 0 && !hashIsZero) {
-                h = Objects.hash(start, stop, bytes);
+                h = Long.hashCode(bytes) + super.hashCode();
                 if (h == 0) {
                     hashIsZero = true;
                 } else {
@@ -146,20 +75,21 @@ public enum ResponseSent {
             if (this == obj) {
                 return true;
             }
-            if (obj == null || this.getClass() != obj.getClass()) {
+            if (obj == null ||
+                this.getClass() != obj.getClass()) {
                 return false;
             }
             var other = (Stats) obj;
-            return this.start == other.start &&
-                   this.stop  == other.stop  &&
-                   this.bytes == other.bytes;
+            return this.bytes == other.bytes &&
+                   this.nanoTimeOnStart() == other.nanoTimeOnStart() &&
+                   this.nanoTimeOnStop()  == other.nanoTimeOnStop();
         }
         
         @Override
         public String toString() {
             return ResponseSent.class.getSimpleName() + '.' + Stats.class.getSimpleName() + "{" +
-                    "start=" + start +
-                    ", stop=" + stop +
+                    "start=" + nanoTimeOnStart() +
+                    ", stop=" + nanoTimeOnStop() +
                     ", bytes=" + bytes + '}';
         }
     }
