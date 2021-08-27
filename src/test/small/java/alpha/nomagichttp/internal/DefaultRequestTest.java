26d915b138112a06b4ea1babea388959f7fa60e4
Martin Andersson <webmaster@martinandersson.com>
2021-08-27 10:14:32 +0200

    Separate attributes tests

--- src/test/small/java/alpha/nomagichttp/internal/DefaultRequestTest.java
+++ src/test/small/java/alpha/nomagichttp/internal/DefaultRequestTest.java
@@ -14,7 +14,6 @@ import java.nio.charset.UnsupportedCharsetException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
 
 import static alpha.nomagichttp.HttpConstants.Version.HTTP_1_1;
 import static alpha.nomagichttp.testutil.Assertions.assertFailed;
@@ -22,7 +21,6 @@ import static alpha.nomagichttp.util.Headers.of;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static java.nio.file.Files.notExists;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Small tests of {@link DefaultRequest}.
@@ -31,9 +29,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  */
 class DefaultRequestTest
 {
-    // Body
-    // ----
-    
     @Test
     void body_toText_happyPath() {
         Request req = createRequest(of(
@@ -93,39 +88,6 @@ class DefaultRequestTest
         assertThat(createEmptyRequest().body().toFile(letsHopeItDoesNotExist)).isCompletedWithValue(0L);
         // Post condition (either test failed legitimately or machine is a pedophile)
         assertThat(notExists(letsHopeItDoesNotExist)).isTrue();
-    }
-    
-    // Attributes
-    // ----
-    
-    @Test
-    void attributes_set_get() {
-        Request r = createEmptyRequest();
-        Object o = r.attributes().set("msg", "hello");
-        assertThat(o).isNull();
-        String n = r.attributes().getAny("msg");
-        assertThat(n).isEqualTo("hello");
-    }
-    
-    @Test
-    void class_cast_exception_immediate() {
-        Request r = createEmptyRequest();
-        r.attributes().set("int", 123);
-        
-        assertThatThrownBy(() -> {
-            String crash = r.attributes().getAny("int");
-        }).isExactlyInstanceOf(ClassCastException.class);
-    }
-    
-    @Test
-    void class_cast_exception_delayed() {
-        Request r = createEmptyRequest();
-        r.attributes().set("int", 123);
-        Optional<String> opt = r.attributes().getOptAny("int");
-        
-        assertThatThrownBy(() -> {
-            String crash = opt.get();
-        }).isExactlyInstanceOf(ClassCastException.class);
     }
     
     private static DefaultRequest createRequest(HttpHeaders headers, String body) {
