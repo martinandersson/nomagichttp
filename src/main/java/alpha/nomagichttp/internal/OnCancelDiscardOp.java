6636ddd2ca7560503b8feda7f2a9afffa23e4851
Martin Andersson <webmaster@martinandersson.com>
2021-12-11 20:06:10 +0100

    Update JavaDoc

--- src/main/java/alpha/nomagichttp/internal/OnCancelDiscardOp.java
+++ src/main/java/alpha/nomagichttp/internal/OnCancelDiscardOp.java
@@ -21,20 +21,24 @@ import static java.lang.Long.MAX_VALUE;
  * Is used by the server's request thread to make sure that if and when the
  * application's body subscription is prematurely cancelled, the read-position
  * in the underlying channel is moved forward so that the next request head
- * parser starts at a valid position. A potential improvement is to compute the
- * number of bytes that needs to be discarded and if this number is sufficient
- * enough; close the channel (re-establishing a new connection assumed to be
- * faster than accepting the rest of a large message that we don't care about).
+ * parser starts at a valid position.
  * 
  * @author Martin Andersson (webmaster at martinandersson.com)
  * @see PooledByteBufferHolder#discard(PooledByteBufferHolder) 
  */
 final class OnCancelDiscardOp extends AbstractOp<PooledByteBufferHolder>
 {
-    // TODO: See JavaDoc. There's room for even more improvement.
-    //       If HttpExchange.prepareForNewExchange() can figure out there will
-    //       be no subsequent exchange from the same channel, then instead
-    //       of discarding - whatever the size - he can just close the channel.
+    /*
+     * TODO
+     * A potential improvement is to compute the number of bytes that needs to
+     * be discarded and if this number is sufficient; close the channel
+     * (re-establishing a new connection assumed to be faster than accepting the
+     * rest of a large message that we don't care about).
+     * 
+     * AND also, if HttpExchange.prepareForNewExchange() can figure out there
+     * will be no subsequent exchange from the same channel, then instead of
+     * discarding - whatever the size - he can just close the channel.
+     */
     
     private volatile boolean discarding;
     
