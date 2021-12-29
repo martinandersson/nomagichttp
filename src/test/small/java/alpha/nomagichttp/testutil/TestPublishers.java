d3c793553c8429366c99434b630fd70f8910dabf
Martin Andersson <webmaster@martinandersson.com>
2021-12-29 20:37:04 +0100

    Add map()

--- src/test/small/java/alpha/nomagichttp/testutil/TestPublishers.java
+++ src/test/small/java/alpha/nomagichttp/testutil/TestPublishers.java
@@ -3,6 +3,7 @@ package alpha.nomagichttp.testutil;
 import java.net.http.HttpRequest.BodyPublisher;
 import java.util.concurrent.Flow;
 import java.util.concurrent.Semaphore;
+import java.util.function.Function;
 
 import static alpha.nomagichttp.util.BetterBodyPublishers.asBodyPublisher;
 import static java.util.Objects.requireNonNull;
@@ -47,20 +48,75 @@ public final class TestPublishers {
      */
     public static BodyPublisher blockSubscriberUntil(Semaphore permit) {
         requireNonNull(permit);
-        return asBodyPublisher(s -> s.onSubscribe(new Flow.Subscription() {
-            @Override
-            public void request(long n) {
-                try {
-                    permit.acquire();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException("Interrupted while waiting on permit.", e);
+        return asBodyPublisher(s ->
+            s.onSubscribe(new Flow.Subscription() {
+                public void request(long n) {
+                    try {
+                        permit.acquire();
+                    } catch (InterruptedException e) {
+                    throw new RuntimeException(
+                        "Interrupted while waiting on permit.", e);
+                    }
                 }
+                public void cancel() {
+                    permit.release();
+                }
+            }));
+    }
+    
+    /**
+     * Map upstream items to downstream items of another type.
+     *
+     * @param <T> the subscribed item type
+     * @param <R> the published item type
+     * @param upstream publisher
+     * @param mapper function
+     * @return a publisher publishing the new resulting type
+     * @throws NullPointerException if any arg is {@code null}
+     */
+    public static <T, R> Flow.Publisher<R> map(
+            Flow.Publisher<? extends T> upstream,
+            Function<? super T, ? extends R> mapper) {
+        return new Operator<>(upstream, mapper);
+    }
+    
+    // AbstractOp will likely be deprecated.
+    // Subscribes up as many times as subscribers subscribe.
+    // No volatile semantics - none needed!
+    private static final class Operator<T, R> implements Flow.Publisher<R>  {
+        private final Flow.Publisher<? extends T> up;
+        private final Function<? super T, ? extends R> f;
+        
+        Operator(Flow.Publisher<? extends T> upstream,
+                 Function<? super T, ? extends R> mapper)
+        {
+            up = requireNonNull(upstream);
+            f  = requireNonNull(mapper);
+        }
+        
+        @Override
+        public void subscribe(Flow.Subscriber<? super R> s) {
+            up.subscribe(new Apply(s));
+        }
+        
+        private final class Apply implements Flow.Subscriber<T> {
+            private final Flow.Subscriber<? super R> d;
+            
+            Apply(Flow.Subscriber<? super R> delegate) {
+                d = requireNonNull(delegate);
             }
             
-            @Override
-            public void cancel() {
-                permit.release();
-            }
-        }));
+            public void onSubscribe(Flow.Subscription s) {
+                d.onSubscribe(s); }
+            
+            public void onNext(T item) {
+                d.onNext(f.apply(item)); }
+            
+            public void onError(Throwable t) {
+                d.onError(t); }
+            
+            public void onComplete() {
+                d.onComplete(); }
+        }
     }
 }
