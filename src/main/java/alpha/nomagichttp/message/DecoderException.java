cd614c641cc22279c90882b5d91c8f96a2fdd2c9
Martin Andersson <webmaster@martinandersson.com>
2022-01-15 20:07:15 +0100

    Update JavaDoc

--- src/main/java/alpha/nomagichttp/message/DecoderException.java
+++ src/main/java/alpha/nomagichttp/message/DecoderException.java
@@ -3,7 +3,10 @@ package alpha.nomagichttp.message;
 import alpha.nomagichttp.handler.ErrorHandler;
 
 /**
- * A byte decoder failed.
+ * A byte decoder failed.<p>
+ * 
+ * A byte decoder decodes a stream of bytes into another stream of bytes, e.g.
+ * HTTP/1.1 dechunking.<p>
  * 
  * The {@link ErrorHandler#DEFAULT default error handler} will translate this
  * exception to a {@link Responses#badRequest() 400 Bad Request}.
