82ee1c444aa7238437e234222eebb4019ad31820
Martin Andersson <webmaster@martinandersson.com>
2023-03-02 06:00:11 +0100

    Fix test

--- src/test/small/java/alpha/nomagichttp/message/ResponseBuilderTest.java
+++ src/test/small/java/alpha/nomagichttp/message/ResponseBuilderTest.java
@@ -17,31 +17,41 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 final class ResponseBuilderTest
 {
     @Test
-    void happy_path() {
+    void happyPath() {
         Response r = builder(200, "OK").build();
         assertThat(r.statusCode()).isEqualTo(200);
         assertThat(r.reasonPhrase()).isEqualTo("OK");
+        assertThat(r.headers().delegate().map()).isEmpty();
         assertSame(r.body(), empty());
     }
     
     @Test
-    void future_changes_does_not_affect_old_builders() {
-        Response.Builder b0 = builder(1).reasonPhrase(""),
-                         b1 = b0.statusCode(2),
-                         b2 = b1.statusCode(3);
-        
-        assertThat(b1.build().statusCode()).isEqualTo(2);
-        assertThat(b2.build().statusCode()).isEqualTo(3);
+    void onlyStatusCode() {
+        Response r = builder(102).build();
+        assertThat(r.statusCode()).isEqualTo(102);
+        assertThat(r.reasonPhrase()).isEqualTo("Unknown");
+        assertThat(r.headers().delegate().map()).isEmpty();
+        assertSame(r.body(), empty());
     }
     
     @Test
-    void header_addHeader_single() {
+    void changesAffectNewInstanceNotOld() {
+        Response.Builder b1 = builder(1).reasonPhrase(""),
+                         b2 = b1.statusCode(2),
+                         b3 = b2.statusCode(3);
+        assertThat(b1.build().statusCode()).isEqualTo(1);
+        assertThat(b2.build().statusCode()).isEqualTo(2);
+        assertThat(b3.build().statusCode()).isEqualTo(3);
+    }
+    
+    @Test
+    void headerAddOne() {
         Response r = builder(-1).addHeaders("k", "v").build();
         assertThat(r.headersForWriting()).containsExactly("k: v");
     }
     
     @Test
-    void header_addHeaders_multi() {
+    void headersAddRepeated() {
         Response r = builder(-1)
             .header(    "k", "v2")
             .addHeader( "k", "v1")
@@ -60,21 +70,7 @@ final class ResponseBuilderTest
     }
     
     @Test
-    void header_empty_key() {
-        var b = builder(-1).header("  ", "<-- empty");
-        assertThatThrownBy(b::build)
-            // From HttpHeaders.of
-            .isExactlyInstanceOf(IllegalStateException.class)
-            .hasNoSuppressedExceptions()
-            .cause()
-                .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasNoSuppressedExceptions()
-                .hasNoCause()
-                .hasMessage("empty key");
-    }
-    
-    @Test
-    void header_empty_value() {
+    void headerEmptyValue() {
         var r = builder(-1).addHeaders(
             "Key 1", "",
             "Key 2", "   ").build();
@@ -84,51 +80,58 @@ final class ResponseBuilderTest
     }
     
     @Test
-    void header_removeHeader() {
+    void headerRemove() {
         Response r = builder(-1).header("k", "v").removeHeader("k").build();
         assertThat(r.headersForWriting()).isEmpty();
     }
     
     @Test
-    void header_replace() {
+    void headerReplace() {
         Response r = builder(-1).header("k", "v1").header("k", "v2").build();
         assertThat(r.headersForWriting()).containsExactly("k: v2");
     }
     
     @Test
-    void informational_response_can_build() {
-        builder(102).build();
+    void headerNameIsRepeatedWithDifferentCasing() {
+        var b = builder(-1).addHeaders("Abc", "X", "abC", "Boom!");
+        assertThatThrownBy(b::build)
+            .isExactlyInstanceOf(IllegalStateException.class)
+            .hasMessage(
+                "java.lang.IllegalArgumentException: duplicate key: abC")
+            .hasNoSuppressedExceptions()
+            .cause()
+                // From HttpHeaders.of
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("duplicate key: abC")
+                .hasNoSuppressedExceptions()
+                .hasNoCause();
     }
     
     @Test
-    void multipleContentLength_withValues() {
-        Response.Builder b = builder(123).addHeaders(
-                "Content-Length", "1",
-                "Content-Length", "2");
-        
+    void headerNameIsEmpty() {
+        var b = builder(-1).header("  ", "<-- empty");
         assertThatThrownBy(b::build)
-                .isExactlyInstanceOf(IllegalStateException.class)
-                .hasMessage("Multiple Content-Length headers.");
-    }
-    
-    @Test
-    void multipleContentLength_noValues() {
-        Response.Builder b = builder(123).addHeaders(
-                "Content-Length", "",
-                "Content-Length", "    ");
-        
-        assertThatThrownBy(b::build)
-                .isExactlyInstanceOf(IllegalStateException.class)
-                .hasMessage("Multiple Content-Length headers.");
+            .isExactlyInstanceOf(IllegalStateException.class)
+            .hasMessage(
+                "java.lang.IllegalArgumentException: empty key")
+            .hasNoSuppressedExceptions()
+            .cause()
+                // From HttpHeaders.of
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasNoSuppressedExceptions()
+                .hasNoCause()
+                .hasMessage("empty key");
     }
     
     @Test
     void connectionCloseOn1XX() {
-        Response.Builder b = builder(123).header("coNnEcTiOn", "cLoSe");
-        
+        var b = builder(123).header("coNnEcTiOn", "cLoSe");
         assertThatThrownBy(b::build)
-                .isExactlyInstanceOf(IllegalStateException.class)
-                .hasMessage("\"Connection: close\" set on 1XX (Informational) response.");
+            .isExactlyInstanceOf(IllegalStateException.class)
+            .hasMessage(
+                "\"Connection: close\" set on 1XX (Informational) response.")
+            .hasNoCause()
+            .hasNoSuppressedExceptions();
     }
     
     // Response.builder() uses a cache, as this is a test we prefer to bypass it
