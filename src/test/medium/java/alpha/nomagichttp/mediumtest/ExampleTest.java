0383b2e366839c376d5b170b03b49ead0ca30be0
Martin Andersson <webmaster@martinandersson.com>
2023-03-18 16:05:23 +0100

    Fix test

--- src/test/medium/java/alpha/nomagichttp/mediumtest/ExampleTest.java
+++ src/test/medium/java/alpha/nomagichttp/mediumtest/ExampleTest.java
@@ -302,8 +302,8 @@ class ExampleTest extends AbstractRealTest
             "HTTP/1.1 102 Processing"                 + CRLF + CRLF +
             
             "HTTP/1.1 200 OK"                         + CRLF +
-            "Content-Length: 5"                       + CRLF +
-            "Content-Type: text/plain; charset=utf-8" + CRLF + CRLF +
+            "Content-Type: text/plain; charset=utf-8" + CRLF +
+            "Content-Length: 5"                       + CRLF + CRLF +
             
             "Done!");
     }
