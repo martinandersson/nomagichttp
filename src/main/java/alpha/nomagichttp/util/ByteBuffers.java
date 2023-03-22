819d58fd311d1bf75c26278f19f8c4105387f30d
Martin Andersson <webmaster@martinandersson.com>
2023-03-22 17:58:20 +0100

    Update JavaDoc

--- src/main/java/alpha/nomagichttp/util/ByteBuffers.java
+++ src/main/java/alpha/nomagichttp/util/ByteBuffers.java
@@ -20,16 +20,18 @@ public final class ByteBuffers
     /**
      * Extracts a {@code byte[]} from the given buffer.<p>
      * 
-     * If the given bytebuffer is backed by an array and the bytebuffer's
-     * remaining content is fully represented by the array, then the backing
-     * array is returned. Otherwise, a new array is created and the remaining
-     * bytes will be transferred without updating the bytebuffer's position.<p>
+     * If the given bytebuffer is backed by an array, is not read-only, and the
+     * bytebuffer's remaining content is fully represented by the array, then
+     * the backing array is returned.<p>
      * 
-     * In other words, neither the buffer nor the array should be modified
-     * unless it is intended that the modification is visible to holders of both.
+     * Otherwise, a new array is created and the remaining bytes will be
+     * transferred without updating the bytebuffer's position.<p>
+     * 
+     * In other words, neither the buffer nor the returned array should be
+     * modified unless it is intended that the modification is visible to
+     * holders of both.
      * 
      * @param buf to extract bytes from
-     * 
      * @return see JavaDoc
      */
     public static byte[] asArray(ByteBuffer buf) {
