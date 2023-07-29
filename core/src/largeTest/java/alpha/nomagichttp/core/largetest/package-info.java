b7a306d46b3b685fd3298d37c68ec87a1e9ccd34
Martin Andersson <webmaster@martinandersson.com>
2023-07-29 19:49:15 +0200

    Update JavaDoc

--- core/src/largeTest/java/alpha/nomagichttp/core/largetest/package-info.java
+++ core/src/largeTest/java/alpha/nomagichttp/core/largetest/package-info.java
@@ -1,5 +1,5 @@
 /**
  * Akin to medium tests - i.e., runs HTTP exchanges on a local port - except the
- * exchanges are big in size or there's many of them running.
+ * exchanges use big amounts of data, or there are many of them running.
  */
 package alpha.nomagichttp.core.largetest;
