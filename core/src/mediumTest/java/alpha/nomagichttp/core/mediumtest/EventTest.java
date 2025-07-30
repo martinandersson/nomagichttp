f117cec4e9665d5ed7add555c946cf9064d6f9e5
Martin Andersson <webmaster@martinandersson.com>
2025-07-30 14:15:52 +0200

    Reduce indentation

--- core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/EventTest.java
+++ core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/EventTest.java
@@ -17,59 +17,60 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 ///  Tests of server-emitted events.
-final class EventTest extends AbstractRealTest {
-        @Test
-        void requestHeadReceived() throws IOException, InterruptedException {
-            engine(RequestHeadReceived.class);
+final class EventTest extends AbstractRealTest
+{
+    @Test
+    void requestHeadReceived() throws IOException, InterruptedException {
+        engine(RequestHeadReceived.class);
+    }
+    
+    @Test
+    void responseSent() throws IOException, InterruptedException {
+        engine(ResponseSent.class);
+    }
+    
+    private void engine(Class<?> eventType)
+            throws IOException, InterruptedException
+    {
+        // Save event locally
+        var eventSink = new ArrayBlockingQueue<AbstractByteCountedStats>(1);
+        server().events().on(eventType, (ev, thing, s) ->
+                eventSink.add((AbstractByteCountedStats) s));
+        
+        final long   beforeReq,
+                     beforeRsp,
+                     afterRsp;
+        final String req = "GET / HTTP/1.1",
+                     rsp;
+        
+        try (var _ = client().openConnection()) {
+            beforeReq = nanoTime();
+            client().write(req);
+            beforeRsp = nanoTime();
+            rsp = client().writeReadTextUntilNewlines(CRLF + CRLF);
+            afterRsp = nanoTime();
         }
         
-        @Test
-        void responseSent() throws IOException, InterruptedException {
-            engine(ResponseSent.class);
-        }
+        assertThat(rsp).isEqualTo(
+            "HTTP/1.1 404 Not Found" + CRLF +
+            "Content-Length: 0"      + CRLF + CRLF);
         
-        private void engine(Class<?> eventType)
-                throws IOException, InterruptedException
-        {
-            // Save event locally
-            var eventSink = new ArrayBlockingQueue<AbstractByteCountedStats>(1);
-            server().events().on(eventType, (ev, thing, s) ->
-                    eventSink.add((AbstractByteCountedStats) s));
-            
-            final long   beforeReq,
-                         beforeRsp,
-                         afterRsp;
-            final String req = "GET / HTTP/1.1",
-                         rsp;
-            
-            try (var _ = client().openConnection()) {
-                beforeReq = nanoTime();
-                client().write(req);
-                beforeRsp = nanoTime();
-                rsp = client().writeReadTextUntilNewlines(CRLF + CRLF);
-                afterRsp = nanoTime();
-            }
-            
-            assertThat(rsp).isEqualTo(
-                "HTTP/1.1 404 Not Found" + CRLF +
-                "Content-Length: 0"      + CRLF + CRLF);
-            
-            assertAwaitHandledAndLoggedExc()
-                    .isExactlyInstanceOf(NoRouteFoundException.class);
-            
-            var stats = eventSink.poll(1, SECONDS);
-            assert stats != null;
-            
-            final long expMaxDur = afterRsp - (
-                eventType == RequestHeadReceived.class ? beforeReq : beforeRsp);
-            assertThat(stats.elapsedNanos()).isBetween(0L, expMaxDur);
-            
-            final long expLen = eventType == RequestHeadReceived.class ?
-                lengthOf(req + CRLF + CRLF) :lengthOf(rsp);
-            assertThat(stats.byteCount()).isEqualTo(expLen);
-        }
+        assertAwaitHandledAndLoggedExc()
+                .isExactlyInstanceOf(NoRouteFoundException.class);
         
-        private static long lengthOf(String message) {
-            return message.getBytes(US_ASCII).length;
-        }
+        var stats = eventSink.poll(1, SECONDS);
+        assert stats != null;
+        
+        final long expMaxDur = afterRsp - (
+            eventType == RequestHeadReceived.class ? beforeReq : beforeRsp);
+        assertThat(stats.elapsedNanos()).isBetween(0L, expMaxDur);
+        
+        final long expLen = eventType == RequestHeadReceived.class ?
+            lengthOf(req + CRLF + CRLF) :lengthOf(rsp);
+        assertThat(stats.byteCount()).isEqualTo(expLen);
+    }
+    
+    private static long lengthOf(String message) {
+        return message.getBytes(US_ASCII).length;
+    }
 }
