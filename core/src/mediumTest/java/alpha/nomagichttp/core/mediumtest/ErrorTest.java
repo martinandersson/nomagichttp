c1ab3f615443c4ff776e348d321c9d0fe6b48115
Martin Andersson <webmaster@martinandersson.com>
2025-08-01 05:25:22 +0200

    Consolidate test cases into HttpExchangeTest

--- core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/ErrorTest.java
+++ core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/ErrorTest.java
@@ -6,7 +6,6 @@ import alpha.nomagichttp.message.IllegalRequestBodyException;
 import alpha.nomagichttp.message.IllegalResponseBodyException;
 import alpha.nomagichttp.message.Request;
 import alpha.nomagichttp.message.Response;
-import alpha.nomagichttp.testutil.IORunnable;
 import alpha.nomagichttp.testutil.functional.AbstractRealTest;
 import alpha.nomagichttp.util.Throwing;
 import org.junit.jupiter.api.Nested;
@@ -14,19 +13,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static alpha.nomagichttp.core.mediumtest.util.TestRequests.get;
-import static alpha.nomagichttp.handler.RequestHandler.GET;
 import static alpha.nomagichttp.handler.RequestHandler.POST;
-import static alpha.nomagichttp.message.Responses.badRequest;
-import static alpha.nomagichttp.message.Responses.noContent;
 import static alpha.nomagichttp.message.Responses.text;
-import static alpha.nomagichttp.testutil.TestConstants.CRLF;
-import static alpha.nomagichttp.util.ScopedValues.channel;
 import static java.lang.System.Logger.Level.DEBUG;
-import static java.lang.System.Logger.Level.ERROR;
-import static java.lang.System.Logger.Level.INFO;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests concerning server errors.<p>
@@ -53,64 +43,6 @@ final class ErrorTest extends AbstractRealTest
     
     @Nested
     class Special {
-        @Test
-        void maxErrorResponses() throws IOException, InterruptedException {
-            server().add("/", GET().apply(_ -> badRequest().toBuilder()
-                    // This header would have caused the server to close the connection,
-                    // but we want to run many "failed" responses
-                    .removeHeaderValue("Connection", "close").build()));
-            
-            IORunnable sendBadRequest = () -> {
-                String rsp = client().writeReadTextUntilNewlines(get());
-                assertThat(rsp).startsWith("HTTP/1.1 400 Bad Request");
-            };
-            
-            final int max = server().getConfig().maxErrorResponses();
-            LOG.log(INFO, () -> "Configured max: " + max);
-            
-            try (var conn = client().openConnection()) {
-                for (int i = max; i > 1; --i) {
-                    if (LOG.isLoggable(INFO)) {
-                        LOG.log(INFO, "Running #" + i);
-                    }
-                    sendBadRequest.run();
-                    assertTrue(conn.isOpen());
-                }
-                LOG.log(INFO, "Running last.");
-                sendBadRequest.run();
-                
-                logRecorder().assertAwait(
-                    DEBUG, "Max number of error responses reached, closing channel.");
-                assertTrue(client().serverClosedOutput());
-                assertTrue(client().serverClosedInput());
-            }
-        }
-        
-        @Test
-        void writingTwoFinalResponses() throws IOException, InterruptedException {
-            server().add("/", GET().apply(_ -> {
-                channel().write(noContent());
-                return text("this won't work");
-            }));
-            String rsp = client().writeReadTextUntilNewlines(
-                "GET / HTTP/1.1"          + CRLF + CRLF);
-            // The first one succeeded
-            assertThat(rsp).isEqualTo(
-                "HTTP/1.1 204 No Content" + CRLF + CRLF);
-            // The second one caused some problems
-            // TODO: See below, is repeated in next test case
-            logRecorder().assertAwaitRemove(
-                    ERROR, """
-                        Response bytes already sent, \
-                        can not handle this exception (closing child).""",
-                    IllegalArgumentException.class)
-                .hasMessage("""
-                    Request processing chain \
-                    both wrote and returned a final response.""")
-                .hasNoCause()
-                .hasNoSuppressedExceptions();
-        }
-        
         /**
          * The error version of
          * {@link ExampleTest.NonPublicExamples#requestTrailers()}.
--- core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/HttpExchangeTest.java
+++ core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/HttpExchangeTest.java
@@ -12,23 +12,33 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static alpha.nomagichttp.core.mediumtest.util.TestRequestHandlers.respondIsBodyEmpty;
+import static alpha.nomagichttp.core.mediumtest.util.TestRequests.get;
 import static alpha.nomagichttp.core.mediumtest.util.TestRequests.post;
 import static alpha.nomagichttp.handler.RequestHandler.GET;
 import static alpha.nomagichttp.handler.RequestHandler.POST;
 import static alpha.nomagichttp.message.Responses.accepted;
+import static alpha.nomagichttp.message.Responses.badRequest;
+import static alpha.nomagichttp.message.Responses.noContent;
 import static alpha.nomagichttp.message.Responses.text;
 import static alpha.nomagichttp.testutil.TestConstants.CRLF;
 import static alpha.nomagichttp.testutil.functional.Environment.isGitHubActions;
 import static alpha.nomagichttp.testutil.functional.Environment.isJitPack;
+import static alpha.nomagichttp.util.ScopedValues.channel;
 import static java.lang.System.Logger.Level.DEBUG;
+import static java.lang.System.Logger.Level.ERROR;
+import static java.lang.System.Logger.Level.INFO;
 import static java.time.Duration.ofMillis;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /// Weird tests that don't really fit anywhere else.
 /// 
 /// @author Martin Andersson (webmaster at martinandersson.com)
 final class HttpExchangeTest extends AbstractRealTest
 {
+    private static final System.Logger LOG
+            = System.getLogger(HttpExchangeTest.class.getPackageName());
+    
     @Nested
     class AppCrash {
         @Test
@@ -198,6 +208,39 @@ final class HttpExchangeTest extends AbstractRealTest
         //       so skipping this for now.
     }
     
+    @Test
+    void maxErrorResponses() throws IOException, InterruptedException {
+        server().add("/", GET().apply(_ -> badRequest().toBuilder()
+                // This header would have caused the server to close the connection,
+                // but we want to run many "failed" responses
+                .removeHeaderValue("Connection", "close").build()));
+        
+        IORunnable sendBadRequest = () -> {
+            String rsp = client().writeReadTextUntilNewlines(get());
+            assertThat(rsp).startsWith("HTTP/1.1 400 Bad Request");
+        };
+        
+        final int max = server().getConfig().maxErrorResponses();
+        LOG.log(INFO, () -> "Configured max: " + max);
+        
+        try (var conn = client().openConnection()) {
+            for (int i = max; i > 1; --i) {
+                if (LOG.isLoggable(INFO)) {
+                    LOG.log(INFO, "Running #" + i);
+                }
+                sendBadRequest.run();
+                assertTrue(conn.isOpen());
+            }
+            LOG.log(INFO, "Running last.");
+            sendBadRequest.run();
+            
+            logRecorder().assertAwait(
+                DEBUG, "Max number of error responses reached, closing channel.");
+            assertTrue(client().serverClosedOutput());
+            assertTrue(client().serverClosedInput());
+        }
+    }
+    
     @Nested
     class MaxRequestSize {
         @Test
@@ -284,5 +327,30 @@ final class HttpExchangeTest extends AbstractRealTest
             .hasNoCause()
             .hasNoSuppressedExceptions()
             .hasMessage("Unsupported Transfer-Encoding: blabla");
+    }
+    
+    @Test
+    void writingTwoFinalResponses() throws IOException, InterruptedException {
+        server().add("/", GET().apply(_ -> {
+            channel().write(noContent());
+            return text("this won't work");
+        }));
+        String rsp = client().writeReadTextUntilNewlines(
+            "GET / HTTP/1.1"          + CRLF + CRLF);
+        // The first one succeeded
+        assertThat(rsp).isEqualTo(
+            "HTTP/1.1 204 No Content" + CRLF + CRLF);
+        // The second one caused some problems
+        // TODO: See below, is repeated in next test case
+        logRecorder().assertAwaitRemove(
+                ERROR, """
+                    Response bytes already sent, \
+                    can not handle this exception (closing child).""",
+                IllegalArgumentException.class)
+            .hasMessage("""
+                Request processing chain \
+                both wrote and returned a final response.""")
+            .hasNoCause()
+            .hasNoSuppressedExceptions();
     }
 }
