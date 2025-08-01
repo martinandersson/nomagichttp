ca72035d9285f72cbeb80e15c020c087081ab15f
Martin Andersson <webmaster@martinandersson.com>
2025-08-01 06:10:42 +0200

    Move charsetPreferenceThroughQ to MessageTest

--- core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/MessageTest.java
+++ core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/MessageTest.java
@@ -29,6 +29,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 // TODO: Run tests using different clients
 final class MessageTest extends AbstractRealTest
 {
+    // "Accept: text/plain; charset=utf-8; q=0.9, text/plain; charset=iso-8859-1"
+    // ISO 8859 wins, coz implicit q = 1
+    @Test
+    void charsetPreferenceThroughQ() throws IOException {
+        server().add("/", GET().apply(req ->
+            text("hello", req)));
+        
+        // Default is UTF-8
+        // (important to keep this here as we need to make sure the next test
+        //  pass for the right reasons)
+        var rsp1 = client().writeReadTextUntil(
+            "GET / HTTP/1.1"                          + CRLF + CRLF, "hello");
+        assertThat(rsp1).isEqualTo(
+            "HTTP/1.1 200 OK"                         + CRLF +
+            "Content-Type: text/plain; charset=utf-8" + CRLF +
+            "Content-Length: 5"                       + CRLF + CRLF +
+            
+            "hello");
+        
+        // Responses.text(String, Request) uses charset from request
+        var rsp2 = client().writeReadTextUntil(
+            "GET / HTTP/1.1"                          + CRLF +
+            "Accept: text/plain; charset=utf-8; q=0.9, " +
+                    "text/plain; charset=iso-8859-1"  + CRLF + CRLF, "hello");
+        assertThat(rsp2).isEqualTo(
+            "HTTP/1.1 200 OK"                              + CRLF +
+            "Content-Type: text/plain; charset=iso-8859-1" + CRLF +
+            "Content-Length: 5"                            + CRLF + CRLF +
+            
+            "hello");
+    }
+    
     /**
      * Can make an HTTP/1.0 request (receives HTTP/1.1 response).<p>
      * 
