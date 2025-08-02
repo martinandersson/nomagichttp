1bc3d0c14b92bbae313c0a3cf034cedfd3c0fc0b
Martin Andersson <webmaster@martinandersson.com>
2025-08-02 06:56:27 +0200

    Update Javadoc reference

--- core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/ClientLifeCycleTest.java
+++ core/src/mediumTest/java/alpha/nomagichttp/core/mediumtest/ClientLifeCycleTest.java
@@ -121,7 +121,7 @@ final class ClientLifeCycleTest extends AbstractRealTest
          * downstream request-line parser, which throws a parse exception, which
          * the base handler does not log, and responds 400 (Bad Request).
          * 
-         * @see HttpExchangeTest.AppCrash#bodyConsumer()
+         * @see AppCrashTest#bodyConsumer()
          */
         @Test
         void receivedPartialHead()
